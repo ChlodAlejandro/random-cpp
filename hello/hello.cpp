4a1982629b9a9968706cbd2737d4dd7eecee4435
Chlod Alejandro <chlodaidanalejandro@hotmail.com>
2019-08-10 22:35:22 +0800

    added hello.cpp
    https://twitter.com/lizzieidio/status/1160195979850612736

---
+++ hello/hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+main() {
+	cout << "Hello!\n\n";
+	
+	cout << "Response: ";
+	
+	string response;
+	cin >> response;
+	
+	
+	cout << "You said \"" + response + "\" back.\n\n";
+	
+	system("pause");
+}
